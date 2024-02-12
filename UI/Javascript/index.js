64ff593489506cb018774c03f6555178a58d8245
Olivier BYIRINGIRO <yuppiegvng@gmail.com>
2024-02-12 10:21:13 +0200

    Made the menu bar functional

--- UI/Javascript/index.js
+++ UI/Javascript/index.js
@@ -1,6 +1,9 @@
 const hamburger = document.querySelector('.hamburger');
-const navlinks = document.querySelector('.navlinks');
-const socials = document.querySelector('.socials');
+const menu = document.querySelector('.menu');
+const logo = document.querySelector('.logo');
+var body = document.body;
+var html = document.documentElement;
+
 
 const menubtn = document.querySelector('.hamb');
 const closebtn = document.querySelector('.close');
@@ -18,8 +21,37 @@ closebtn.classList.add('hidden');
 menubtn.addEventListener('click', toogleMenu);
 closebtn.addEventListener('click', toogleMenu);
 
+//by default menu will not be displayed
+menu.style.display = 'none';
 
-// hamburger.addEventListener('click', () => {
-//     navlinks.classList.toggle('hide');
-//     socials.classList.toggle('hide');
-// })
+
+//if menu button is clicked
+menubtn.addEventListener('click', () => {
+    //display the meny
+    menu.style.display = 'flex';
+
+    //position the logo and the menu bar
+    hamburger.style.position = 'fixed';
+    hamburger.style.left = '80%';
+    logo.style.position = 'fixed';
+
+    //disable the scrolling
+    body.style.overflowY = 'hidden';
+    html.style.overflowY = 'hidden';
+})
+
+
+
+//if close button is clicked
+closebtn.addEventListener('click', () => {
+    //set the meny to display none
+    menu.style.display = 'none';
+
+    //position the logo and the menu bar as they were
+    hamburger.style.position = 'static';
+    logo.style.position = 'static';
+
+     //enable scrolling again
+    body.style.overflowY = 'scroll';
+    html.style.overflowY = 'scroll';
+})

a654e3accf817bf5bdbeeafa154e965d78b73078
Olivier BYIRINGIRO <yuppiegvng@gmail.com>
2024-02-15 09:24:07 +0200

    Added the function to validate email

--- UI/Javascript/signup-login-formvalidations.js
+++ UI/Javascript/signup-login-formvalidations.js
@@ -19,3 +19,23 @@ myForm.addEventListener('submit', function(event){
 })
 
 
+//function to validate the email input
+function validateEmail(email){
+    let emailLabel = document.getElementById('emaillabel');
+    let emailPlaceholder = document.getElementById('emailplaceholder');
+
+    let atPosition = email.indexOf('@');
+    let dotPosition = email.lastIndexOf('.');
+
+    if(atPosition < 1 || dotPosition < atPosition + 2 || dotPosition + 2 >= email.length){
+        emailLabel.style.borderBottomColor = '#c80202';
+        emailPlaceholder.style.color = '#c80202';
+        emailPlaceholder.innerText = 'Please enter a valid email';
+    } else{
+        emailLabel.style.borderBottomColor = '#02c837';
+        emailPlaceholder.style.color = '#02c837';
+        emailPlaceholder.innerText = 'Email';
+        emailValidated = true;
+    }
+
+}

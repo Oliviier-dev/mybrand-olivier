7d3515e5c2d6dd596740e8c2c6399d0494f0abee
Olivier BYIRINGIRO <yuppiegvng@gmail.com>
2024-02-21 07:47:59 +0200

    Implemented the functionality to change the home button to admin dashboard page

--- UI/Javascript/login-check.js
+++ UI/Javascript/login-check.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getAuth, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+import { getFirestore, doc, getDoc } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
 document.addEventListener('DOMContentLoaded', function() {
 
@@ -17,8 +18,11 @@ const firebaseConfig = {
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
 const auth = getAuth(firebaseApp);
+const db = getFirestore(firebaseApp);
 
 let loginoutButtons = document.querySelectorAll('.loginout-btn');
+let homeorAdminLink = document.querySelectorAll('.homeOrAdmin');
+// console.log(homeorAdminLink);
 // console.log(loginoutButtons);
 
 
@@ -28,6 +32,32 @@ onAuthStateChanged(auth, (user) => {
         // User is signed in.
         // console.log("User is signed in:", user);
         // You can access user information like user.uid, user.email, etc.
+        const userRef = doc(db, 'users', user.uid);
+        getDoc(userRef).then((docSnapshot) => {
+            if (docSnapshot.exists()) {
+                const userData = docSnapshot.data();
+                const userRole = userData.role;
+                
+                // changing the home button to the admin dashboard page
+                if(userData.role == 'admin'){
+                    for (let i = 0; i < homeorAdminLink.length; i++) {
+                        let anchorTag = homeorAdminLink[i].querySelector('a');
+                        anchorTag.innerHTML = 'Admin';
+                        // anchor.setAttribute('href', 'UI/pages/adminpage.html');
+                        if (window.location.href.includes("index.html")) {
+                            anchorTag.href = "UI/pages/adminpage.html";
+                        } else {
+                            anchorTag.href = "adminpage.html";
+                        }
+                    }
+                }
+               
+            } else {
+                console.error("User data not found");
+            }
+        }).catch((error) => {
+            console.error("Error fetching user data:", error);
+        });
 
         for (let i = 0; i < loginoutButtons.length; i++) {
             let anchorTag = loginoutButtons[i].querySelector('a');
@@ -39,8 +69,13 @@ onAuthStateChanged(auth, (user) => {
 
                     //Sign-out successful.
                     // console.log("User signed out successfully.");
+                    if (window.location.href !== "../../index.html"){
+                        window.location.href = "index.html";
+                    } else{
+                        window.location.href = "../../index.html";
+                    }
 
-                    window.location.href = "../../index.html";
+                    
 
                 }).catch((error) => {
                     // An error happened.

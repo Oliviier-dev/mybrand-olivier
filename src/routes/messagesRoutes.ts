cd675d79402d2fb21a2f86376a55aa6110770805
Olivier BYIRINGIRO <yuppiegvng@gmail.com>
2024-02-28 14:40:41 +0200

    Imported the correct file

--- src/routes/messagesRoutes.ts
+++ src/routes/messagesRoutes.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response, NextFunction } from 'express';
-import Message from '../models/usemessages';
+import Message from '../models/usermessages';
 
 const router = express.Router();
 

9f9cfd95cd6d4e057f7208d3365bccd3679c9229
Olivier BYIRINGIRO <yuppiegvng@gmail.com>
2024-02-28 14:39:27 +0200

    Added the user messages model

---
+++ src/models/usemessages.ts
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const messagesSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    message: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+
+// Create and export Message model
+const Message = mongoose.model('Message', messagesSchema);
+export default Message;

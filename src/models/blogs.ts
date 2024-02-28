5648820624a55456c7f3da1a067268b21a213b7c
Olivier BYIRINGIRO <yuppiegvng@gmail.com>
2024-02-28 14:14:13 +0200

    Added the blog model

---
+++ src/models/blogs.ts
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const blogSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    snippet: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    imageUrl: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    },
+    body: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+
+// Create and export Blog model
+const Blog = mongoose.model('Blog', blogSchema);
+export default Blog;

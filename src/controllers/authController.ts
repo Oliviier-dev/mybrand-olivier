28a29e627e951a69087dbc078ae4e5006ebeeccd
Olivier BYIRINGIRO <yuppiegvng@gmail.com>
2024-02-29 07:16:17 +0200

    Created the user in the db

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+const User = require('../models/user');
 
 module.exports.signup_get = (req: Request, res: Response) => {
     res.send('Signup get');
@@ -8,10 +9,19 @@ module.exports.login_get = (req: Request, res: Response) => {
     res.send('login get');
 }
 
-module.exports.signup_post = (req: Request, res: Response) => {
-    res.send('New signup');
+module.exports.signup_post = async(req: Request, res: Response) => {
+    const { email, password } = req.body;
+    
+    try {
+        const user = await User.create({ email, password });
+        res.status(201).json(user);
+    } catch (error) {
+        console.log(error);
+        res.status(400).send('error, user not created');
+    }
+
 }
 
-module.exports.login_post = (req: Request, res: Response) => {
+module.exports.login_post = async(req: Request, res: Response) => {
     res.send('user login');
 }

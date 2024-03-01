898f8c5e1dc4f4a9f67c0bfb9822342787f8cc0b
Olivier BYIRINGIRO <yuppiegvng@gmail.com>
2024-03-01 18:36:07 +0200

    Secured the messages routes

--- src/routes/messagesRoutes.ts
+++ src/routes/messagesRoutes.ts
@@ -1,16 +1,22 @@
 import express, { Request, Response, NextFunction } from 'express';
 import Message from '../models/usermessages';
+const { requireAuth, isAdmin } = require('../middleware/authmiddleware')
 
 const router = express.Router();
 
 // Get a list of messages from the db
-router.get('/usermessages', async (req: Request, res: Response, next: NextFunction) => {
+router.get('/usermessages', requireAuth , isAdmin, async (req: Request, res: Response, next: NextFunction) => {
     try {
         const messages = await Message.find({});
         res.send(messages);
     } catch (err) {
         next(err);
     }
+});
+
+router.use((err: any, req: Request, res: Response, next: NextFunction) => {
+        // Return a 401 Unauthorized response if authentication fails
+        res.status(401).json({ error: 'Unauthorized: Please log in.' });
 });
 
 // Add a new message to the db
@@ -24,7 +30,7 @@ router.post('/contactme', async (req: Request, res: Response, next: NextFunction
 });
 
 // Delete a message from the db
-router.delete('/usermessages/:id', async (req: Request, res: Response, next: NextFunction) => {
+router.delete('/usermessages/:id', requireAuth, isAdmin, async (req: Request, res: Response, next: NextFunction) => {
     try {
         const deletedMessage = await Message.findByIdAndDelete({ _id: req.params.id });
         res.send(deletedMessage);

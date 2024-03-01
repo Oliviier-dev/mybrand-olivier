5bfe57f2340268f1ab4bad793cdbc09bb4863175
Olivier BYIRINGIRO <yuppiegvng@gmail.com>
2024-03-01 18:34:39 +0200

    Added the liking route

--- src/routes/commentsRoutes.ts
+++ src/routes/commentsRoutes.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import { createComment, getCommentsByBlogId } from '../controllers/commentController';
+import { likeBlog } from '../controllers/likesController';
+const { requireAuth, isAdmin } = require('../middleware/authmiddleware')
 
 const router = express.Router();
 
@@ -7,6 +9,11 @@ const router = express.Router();
 router.post("/blogs/:blogId/comments", createComment);
 
 // LIST COMMENTS
-router.get("/blogs/:blogId/comments", getCommentsByBlogId);
+router.get("/blogs/:blogId/comments", requireAuth, isAdmin, getCommentsByBlogId);
+
+
+//liking a blog
+router.post("/blogs/:blogId/like", likeBlog);
+
 
 export default router;

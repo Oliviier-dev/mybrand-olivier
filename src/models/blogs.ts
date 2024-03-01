6bc7f06873d8b89ac0c994622749f8a4b71dd436
Olivier BYIRINGIRO <yuppiegvng@gmail.com>
2024-03-01 18:36:38 +0200

    Added the likes in the blog object

--- src/models/blogs.ts
+++ src/models/blogs.ts
@@ -25,6 +25,10 @@ const blogSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    likes: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Like'
+    }],
     comments: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Comment'

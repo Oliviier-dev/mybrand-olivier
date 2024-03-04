061eec86cffeeb335adec3ad1fe7fbbf4fc259d5
Olivier BYIRINGIRO <yuppiegvng@gmail.com>
2024-03-04 08:03:12 +0200

    Added the function to check for validations on the authroutes

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,12 +1,33 @@
-const { Router } = require('express');
+import { Router, Request, Response } from "express";
 const authController = require('../controllers/authController');
+import schemaValidator from "../middleware/schemaValidator";
 
 const router = Router();
 
-router.get('/signup', authController.signup_get);
-router.post('/signup', authController.signup_post);
-router.get('/login', authController.login_get);
-router.post('/login', authController.login_post);
+// router.get('/signup', authController.signup_get);
+// router.post('/signup', authController.signup_post);
+// router.get('/login', authController.login_get);
+// router.post('/login', authController.login_post);
+// router.post('/logout', authController.logout);
+
+router.post(
+    "/signup",
+    schemaValidator("/signup"),
+    authController.signup_post,
+    (req: Request, res: Response) => {
+      return res.send("Sign up complete");
+    }
+);
+
+router.post(
+    "/login",
+    schemaValidator("/signup"),
+    authController.login_post,
+    (req: Request, res: Response) => {
+      return res.send("Logged in successfully");
+    }
+);
+
 router.post('/logout', authController.logout);
 
 module.exports = router;

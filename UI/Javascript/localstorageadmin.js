d1193acbc9a7753988b60547620c6a605e065d26
Olivier BYIRINGIRO <yuppiegvng@gmail.com>
2024-03-14 14:55:30 +0200

    Implemented functionality to fetch the blogs and messages on the admin dashboard

--- UI/Javascript/localstorageadmin.js
+++ UI/Javascript/localstorageadmin.js
@@ -26,87 +26,162 @@ function displayUserMessages(){
     // let allMessages = document.querySelector('.allmessages');
     let pagesMessages = document.querySelector('.pagesmessages');
 
-    let messages = JSON.parse(localStorage.getItem('userMessages')) || [];
+    //let messages = JSON.parse(localStorage.getItem('userMessages')) || [];
     // console.log(messages);
+    let messages;
 
-    for(let i = 0; i < messages.length; i++){
-        // console.log(messages[i]);
-        let messageDiv = document.createElement('div');
-        messageDiv.classList.add('message');
-
-        let messageDesc = document.createElement('div');
-        messageDesc.classList.add('messagedesc');
-
-        let messageImage = document.createElement('div');
-        messageImage.classList.add('image');
-
-        let contentDiv = document.createElement('div');
-
-        let headingThree = document.createElement('h3');
-        let headingFour = document.createElement('h4');
-        let para = document.createElement('p');
-
-        headingThree.innerText = messages[i][0];
-        headingFour.innerText = messages[i][1];
-        para.innerText = messages[i][2];
-
-
-        contentDiv.append(headingThree);
-        contentDiv.append(headingFour);
-        contentDiv.append(para);
-
-        messageDesc.append(messageImage);
-        messageDesc.append(contentDiv);   
-        
-        messageDiv.append(messageDesc);
-
-        pagesMessages.before(messageDiv);
+    function getCookie(name) {
+        const cookieString = document.cookie;
+        const cookies = cookieString.split('; ');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i];
+            const [cookieName, cookieValue] = cookie.split('=');
+            if (cookieName === name) {
+                return cookieValue;
+            }
+        }
+        return null;
     }
+
+    var tokenCookie = getCookie('jwt');
+    //console.log(tokenCookie);
+
+
+    fetch('https://mybrand-olivier-production.up.railway.app/api/usermessages', {
+        headers: {
+            'Authorization': `Bearer ${tokenCookie}`,
+            'Content-Type': 'application/json'
+          }
+    })
+    .then(data => {
+        return data.json();
+    })
+    .then(data => {
+        console.log(data);
+        messages = data;
+
+        for(let i = 0; i < messages.length; i++){
+            // console.log(messages[i]);
+            let messageDiv = document.createElement('div');
+            messageDiv.classList.add('message');
+    
+            let messageDesc = document.createElement('div');
+            messageDesc.classList.add('messagedesc');
+    
+            let messageImage = document.createElement('div');
+            messageImage.classList.add('image');
+    
+            let contentDiv = document.createElement('div');
+    
+            let headingThree = document.createElement('h3');
+            let headingFour = document.createElement('h4');
+            let para = document.createElement('p');
+    
+            headingThree.innerText = messages[i].name;
+            headingFour.innerText = messages[i].email;
+            para.innerText = messages[i].message;
+    
+    
+            contentDiv.append(headingThree);
+            contentDiv.append(headingFour);
+            contentDiv.append(para);
+    
+            messageDesc.append(messageImage);
+            messageDesc.append(contentDiv);   
+            
+            messageDiv.append(messageDesc);
+    
+            pagesMessages.before(messageDiv);
+        }
+
+    })
+    .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+    });
 }
 
 
 
 function displayBlogsAdmin(){
     let pagesBlogs = document.querySelector('.pagesBlogs');
-    let blogs = JSON.parse(localStorage.getItem('Blogs')) || [];
+    //let blogs = JSON.parse(localStorage.getItem('Blogs')) || [];
 
-    for(let i = 0; i < blogs.length; i++){
 
-        let blogDiv = document.createElement('div');
-        blogDiv.classList.add('blog');
-        
-        let blogDesc = document.createElement('div');
-        blogDesc.classList.add('desc');
 
-        let headingThree = document.createElement('h3');
-        let para = document.createElement('p');
+    let blogs;
 
-        blogDesc.append(headingThree);
-        blogDesc.append(para);
-
-        headingThree.innerText = blogs[i].title;
-        para.innerText = blogs[i].snippet;
-
-        let buttonsDiv = document.createElement('div');
-        buttonsDiv.classList.add('buttons');
-
-        let editbutton = document.createElement('button');
-        editbutton.innerText = 'Edit';
-        editbutton.classList.add('editBlogbtn');
-
-        let deletebutton = document.createElement('button');
-        deletebutton.innerText = 'Delete';
-        deletebutton.classList.add('deleteBlogbtn');
-
-        buttonsDiv.append(editbutton);
-        buttonsDiv.append(deletebutton);
-
-        blogDiv.append(blogDesc);   
-        blogDiv.append(buttonsDiv);  
-        
-        pagesBlogs.before(blogDiv);
-
+    function getCookie(name) {
+        const cookieString = document.cookie;
+        const cookies = cookieString.split('; ');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i];
+            const [cookieName, cookieValue] = cookie.split('=');
+            if (cookieName === name) {
+                return cookieValue;
+            }
+        }
+        return null;
     }
+
+    var tokenCookie = getCookie('jwt');
+    //console.log(tokenCookie);
+
+
+    fetch('https://mybrand-olivier-production.up.railway.app/api/blog/blogs', {
+        headers: {
+            'Authorization': `Bearer ${tokenCookie}`,
+            'Content-Type': 'application/json'
+          }
+    })
+    .then(data => {
+        return data.json();
+    })
+    .then(data => {
+        console.log(data);
+        blogs = data;
+
+        for(let i = 0; i < blogs.length; i++){
+
+            let blogDiv = document.createElement('div');
+            blogDiv.classList.add('blog');
+            
+            let blogDesc = document.createElement('div');
+            blogDesc.classList.add('desc');
+    
+            let headingThree = document.createElement('h3');
+            let para = document.createElement('p');
+    
+            blogDesc.append(headingThree);
+            blogDesc.append(para);
+    
+            headingThree.innerText = blogs[i].title;
+            para.innerText = blogs[i].snippet;
+    
+            let buttonsDiv = document.createElement('div');
+            buttonsDiv.classList.add('buttons');
+    
+            let editbutton = document.createElement('button');
+            editbutton.innerText = 'Edit';
+            editbutton.classList.add('editBlogbtn');
+    
+            let deletebutton = document.createElement('button');
+            deletebutton.innerText = 'Delete';
+            deletebutton.classList.add('deleteBlogbtn');
+    
+            buttonsDiv.append(editbutton);
+            buttonsDiv.append(deletebutton);
+    
+            blogDiv.append(blogDesc);   
+            blogDiv.append(buttonsDiv);  
+            
+            pagesBlogs.before(blogDiv);
+    
+        }
+
+    })
+    .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+    });
 
 }
 

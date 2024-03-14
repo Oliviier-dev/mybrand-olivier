1fdd8fab894f1e6454cc60c7b70331429fec92f5
Olivier BYIRINGIRO <yuppiegvng@gmail.com>
2024-03-14 15:47:05 +0200

    Implemented the function to create the new blog to the db

--- UI/Javascript/createnewblogstorage.js
+++ UI/Javascript/createnewblogstorage.js
@@ -15,40 +15,105 @@ createBlog.addEventListener('submit', function(e){
 })
 
 function storeBlogData(){
-    let existingBlogs = JSON.parse(localStorage.getItem('Blogs')) || [];
+    //let existingBlogs = JSON.parse(localStorage.getItem('Blogs')) || [];
 
-    const createdAT = new Date().toLocaleDateString('en-GB');
+
+    function getCookie(name) {
+        const cookieString = document.cookie;
+        const cookies = cookieString.split('; ');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i];
+            const [cookieName, cookieValue] = cookie.split('=');
+            if (cookieName === name) {
+                return cookieValue;
+            }
+        }
+        return null;
+    }
+
+    var tokenCookie = getCookie('jwt');
+
+
+    //const createdAT = new Date().toLocaleDateString('en-GB');
 
     let newBlog = {
         title: blogTitle.value, 
         snippet: blogSnippet.value, 
         category: blogCategory.value, 
-        image: blogImage.value,
+        imageUrl: blogImage.value,
         author: blogAuthor.value,
-        createdAT: createdAT,
         body: blogBody.value ,
-        comments: JSON.parse(localStorage.getItem('editBlogComments')) || []
+        //comments: JSON.parse(localStorage.getItem('editBlogComments')) || []
     }
 
-    existingBlogs.push(newBlog);
+    //existingBlogs.push(newBlog);
 
-    localStorage.setItem('Blogs', JSON.stringify(existingBlogs));
+    //localStorage.setItem('Blogs', JSON.stringify(existingBlogs));
 
-    blogTitle.value = '';    
-    blogSnippet.value = '';    
-    blogCategory.value = '';    
-    blogImage.value = '';    
-    blogAuthor.value = '';    
-    blogBody.value = '';
-    localStorage.removeItem('editBlogComments');
-    
-    
-    setTimeout(function() {
-        notificationsBar.classList.add('visible');
-    
+
+
+    fetch('https://mybrand-olivier-production.up.railway.app/api/blog/createnew', {
+            method: 'POST',
+            headers: {
+                'Authorization': `Bearer ${tokenCookie}`,
+                'content-Type' : 'application/json'
+            },
+            body: JSON.stringify(newBlog)
+    })
+    .then(response => {
+        if(!response.ok) {
+
+            notificationsBar.innerHTML = `<span class="material-symbols-outlined circle error">error</span>An error Occured`;
+            setTimeout(function() {
+                notificationsBar.classList.add('visible');
+        
+                setTimeout(function() {
+                    notificationsBar.classList.remove('visible');
+                }, 2000);
+            }, 1000); 
+
+        }
+        return response.json();
+    })
+    .then(data => {
+
+        if(data.status === 'failed'){
+
+            notificationsBar.innerHTML = `<span class="material-symbols-outlined circle error">error</span>All Fields are required`;
+            setTimeout(function() {
+                notificationsBar.classList.add('visible');
+        
+                setTimeout(function() {
+                    notificationsBar.classList.remove('visible');
+                }, 2000);
+            }, 1000); 
+
+        } else {
+            blogTitle.value = '';    
+            blogSnippet.value = '';    
+            blogCategory.value = '';    
+            blogImage.value = '';    
+            blogAuthor.value = '';    
+            blogBody.value = '';
+            //localStorage.removeItem('editBlogComments');
+            
+            window.location.href = "adminpage.html";
+        }
+
+        console.log('Message sent:', data);
+    })
+    .catch(error => {
+
+        notificationsBar.innerHTML = `<span class="material-symbols-outlined circle error">error</span>An error`;
         setTimeout(function() {
-            notificationsBar.classList.remove('visible');
-        }, 2000);
-    }, 1000); 
+            notificationsBar.classList.add('visible');
+    
+            setTimeout(function() {
+                notificationsBar.classList.remove('visible');
+            }, 2000);
+        }, 1000); 
+
+        console.error('There was a problem sending the message:', error);
+    });
 
 }

050eaf21625887faaa8b198271fcf9ed72393616
Olivier BYIRINGIRO <yuppiegvng@gmail.com>
2024-03-18 15:03:16 +0200

    Secured the adminpage and createnewblog page to be accessed by admins only

--- UI/Javascript/login-check.js
+++ UI/Javascript/login-check.js
@@ -1,5 +1,39 @@
 document.addEventListener('DOMContentLoaded', function() {
 
+    function getCookie(name) {
+        const cookieString = document.cookie;
+        const cookies = cookieString.split('; ');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i];
+            const [cookieName, cookieValue] = cookie.split('=');
+            if (cookieName === name) {
+                return decodeURIComponent(cookieValue);
+            }
+        }
+        return null;
+    }
+
+    // Check if the user is on an admin-only page
+    if (window.location.pathname.includes("adminpage.html") || window.location.pathname.includes("createnewblog.html")) {
+        // Check if user has admin JWT
+        var tokenCookie = getCookie('jwt');
+        if (!tokenCookie) {
+            window.location.href = "../../index.html"; // Redirect to home page
+        } else {
+            // Decode the JWT token payload
+            var parts = tokenCookie.split('.');
+            var payload = parts[1];
+            var decodedPayload = JSON.parse(atob(payload));
+            console.log("Decoded Payload:", decodedPayload.role);
+
+            if (decodedPayload.role !== 'admin') {
+                window.location.href = "../../index.html"; // Redirect to home page
+            }
+        }
+    }
+
+
+
 let loginoutButtons = document.querySelectorAll('.loginout-btn');
 let homeorAdminLink = document.querySelectorAll('.homeOrAdmin');
 
